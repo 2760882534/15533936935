fe2a68fe0e32eb6d5cad6dd9ee26c9efb4e8fb9e
2760882534@qq.com <2760882534@qq.com>
2021-01-05 18:08:59 +0800

    add 1

---
+++ 1/数组倒序.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define NM
+int main(void)
+{
+	int a[N], i;
+	printf("ENTER %d:" N);
+	for(i = 0; i <= N; i++)
+	scanf("%d", &a[i]);
+	printf("in reverse: ");
+	for(i = N; i >=0; i--)
+	printf("%d", a[i]);
+	return 0;
+}
